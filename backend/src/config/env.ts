29b59d8546cbd7391ff2fc7a5416d625c59416d4
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2024-04-07 17:16:13 -0300

    feat: setting up backend

---
+++ backend/src/config/env.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+
+const initialEnv = dotenv.config();
+dotenvExpand.expand(initialEnv);
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
+  PORT: z.string().regex(/^\d+$/).transform(Number).default("3333"),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  TOKEN_COOKIE_NAME: z.string(),
+  REFRESH_TOKEN_COOKIE_NAME: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+if (_env.success === false) {
+  console.error(_env.error.format());
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
