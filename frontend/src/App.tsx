4262e915e667806c9a694e58694f2e9b23a42f20
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2024-04-10 14:30:07 -0300

    feat: iniciando front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,59 @@
+
+import './App.css'
+import { ForceGraph2D } from 'react-force-graph'
+
+function App() {
+  const data = {
+    "nodes": [
+      {
+        "id": "a95828fc-dca9-419b-af5c-393ccc1b3327"
+      },
+      {
+        "id": "e1af9169-1b3e-471a-8165-09d0429402fe"
+      },
+      {
+        "id": "c531c4cd-ca23-4344-a21a-f0cbc7f4223c"
+      },
+      {
+        "id": "06506103-f0f4-474c-8eea-c90ed26113ff"
+      },
+      {
+        "id": "0e5ee8f6-46d7-4cf3-ae55-d0c9141b5333"
+      }
+    ],
+    "links": [
+      {
+        "source": "a95828fc-dca9-419b-af5c-393ccc1b3327",
+        "target": "e1af9169-1b3e-471a-8165-09d0429402fe"
+      },
+      {
+        "source": "e1af9169-1b3e-471a-8165-09d0429402fe",
+        "target": "c531c4cd-ca23-4344-a21a-f0cbc7f4223c"
+      },
+      {
+        "source": "e1af9169-1b3e-471a-8165-09d0429402fe",
+        "target": "06506103-f0f4-474c-8eea-c90ed26113ff"
+      },
+      {
+        "source": "e1af9169-1b3e-471a-8165-09d0429402fe",
+        "target": "0e5ee8f6-46d7-4cf3-ae55-d0c9141b5333"
+      }
+    ]
+  }
+
+  return (
+    <>
+     <ForceGraph2D
+      graphData={data}
+      linkCurvature={0.2}
+      linkColor={() => 'yellow'}
+      nodeColor={() => 'orange'}
+      backgroundColor='#020720'
+      linkDirectionalParticles={0.5}
+    />
+
+    </>
+  )
+}
+
+export default App
